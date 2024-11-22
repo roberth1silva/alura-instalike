177e02aa45798e9177f36a5f675a7d6f5b2c0a25
Roberth1silva <roberth1silva@hotmail.com>
2024-11-21 22:17:23 -0300

    Aula 4

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,9 +1,23 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsControllers.js";
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsControllers.js";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, "uploads/");
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({dest:"./uploads", storage});
 
 const routes = (app) => {
     app.use(express.json());
     app.get("/posts", listarPosts);
+    app.post("/posts", postarNovoPost);
+    app.post("/upload", upload.single("imagem"), uploadImagem);
 }
 
 export default routes;
