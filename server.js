9d1dc389b78c116b8937323f28fc44011a1e6515
Roberth1silva <roberth1silva@hotmail.com>
2024-11-22 22:13:46 -0300

    Aula 5

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import routes from "./src/routes/postsRoutes.js";
 
-
 const app = express();
+app.use(express.static("uploads"));
 routes(app);
 
 app.listen(3000, () => {
--- src/controllers/postsControllers.js
+++ src/controllers/postsControllers.js
@@ -1,5 +1,6 @@
-import { getTodosPosts, criarPost } from "../model/postsModel.js";
+import { getTodosPosts, criarPost, atualizarPost } from "../model/postsModel.js";
 import fs from "fs";
+import gerarDescricaoComGemini from "../services/geminiService.js";
 
 export async function listarPosts(req, res) {
     const posts = await getTodosPosts();
@@ -29,6 +30,27 @@ export async function uploadImagem(req, res) {
         const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
         fs.renameSync(req.file.path, imagemAtualizada);
         res.status(200).json(postCriado);
+    }    
+    catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisição"});
+    }
+}
+
+export async function atualizarNovoPost(req, res) {
+    const id = req.params.id;
+    const urlImagem = `http://localhost:3000/${id}.png`;
+    
+    try {
+        const imgBuffer = fs.readFileSync(`uploads/${id}.png`);
+        const descricao = await gerarDescricaoComGemini(imgBuffer);
+        const post = {
+            imgUrl: urlImagem,
+            descricao: descricao,
+            alt: req.body.alt
+        }        
+        const postAtualizado = await atualizarPost(id, post);
+        res.status(200).json(postAtualizado);
     }    
     catch(erro) {
         console.error(erro.message);
